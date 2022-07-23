9fda46b3d2eaebf4e7cb7b2e8bba97cba3fa1c00
JustGimli <aleksnermer@gmail.com>
2022-07-23 22:39:35 +0300

    0.05

--- main_core/core_app.py
+++ main_core/core_app.py
@@ -1,11 +1,13 @@
 from flask import *
 from configyre.config import *
+import sys
 
 app = Flask(__name__)
 app.config.from_object(testingConfig)
 
 @app.route('/')
 def main():
+    print(sys.path)
     return render_template('main.html')
 
 if __name__ == '__main__':
