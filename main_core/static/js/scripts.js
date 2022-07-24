6784b4446a4b020ee9486787335784c136226715
JustGimli <aleksnermer@gmail.com>
2022-07-24 16:14:08 +0300

    0.06

--- main_core/static/js/scripts.js
+++ main_core/static/js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    alert('asd')
-})
+    $('nav a[href^="#"]').click(function(){
+        var scroll_el = $(this).attr('href');
+        var destination = $(scroll_el).offset().top;
+        if ($(scroll_el).length != 0){
+            $('html, body').animate({
+                scrollTop: destination
+            },400);
+        }
+        return false;
+    });
+
+});
