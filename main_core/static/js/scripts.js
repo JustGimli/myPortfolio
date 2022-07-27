016ace2f590a8b4d3ec5847d381f2f61923ba361
JustGimli <aleksnermer@gmail.com>
2022-07-27 20:21:41 +0300

    0.07

--- main_core/static/js/scripts.js
+++ main_core/static/js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
+
+
     $('nav a[href^="#"]').click(function(){
         var scroll_el = $(this).attr('href');
-        var destination = $(scroll_el).offset().top;
+        var destination = $(scroll_el).offset().top - 60;
         if ($(scroll_el).length != 0){
+            console.log(destination+60)
             $('html, body').animate({
                 scrollTop: destination
-            },400);
+            },400 );
+           
         }
         return false;
     });
 
+
+    // var profile = $( ".profile-h1" ).offset()
+    // var experiense = $( ".experiense" ).offset()
+    // var abilities = $( ".abilities" ).offset()
+    // var Contact = $( "#Contact" ).offset()
+    // if (profile < $('html').offset() < experiense) {
+    //     $('nav li[href="#profile"]').css('backgroun-color', 'blue')
+    //     alert('[eq')
+    // }
 });
