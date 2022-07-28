15065cd87303506a9a641e51f415fff1f5e72a02
JustGimli <aleksnermer@gmail.com>
2022-07-28 20:16:57 +0300

    0.09

--- main_core/static/js/scripts.js
+++ main_core/static/js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
     $('nav li[href^="#"]').click(function(){
         var scroll_el = $(this).attr('href');
-        var destination = $(scroll_el).offset().top - 60;
+        var destination = $(scroll_el).offset().top;
         if ($(scroll_el).length != 0){
             $('html, body').animate({
                 scrollTop: destination
