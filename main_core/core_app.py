1c1372bf2ba46115877db47355adbfff51c445bf
JustGimli <aleksnermer@gmail.com>
2022-07-30 21:35:03 +0300

    0.3

--- main_core/core_app.py
+++ main_core/core_app.py
@@ -1,12 +1,46 @@
 from flask import *
 from configyre.config import *
-import sys
+from sqlite3 import *
 
 app = Flask(__name__)
 app.config.from_object(config)
 
+
+
+def connect_db():
+
+    con = connect('db/article.db')
+    con.row_factory = Row
+
+    return con
+
+
+def init_db():
+
+    with app.app_context():
+        db = get_db()
+
+        with app.open_resource('db/shem.sql','r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+
+    if not hasattr(g,'sqlite_db'):
+        g.sqlite_db = connect_db()
+        return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 @app.route('/')
 def main():
+    init_db()
     return render_template('main.html')
 
 
