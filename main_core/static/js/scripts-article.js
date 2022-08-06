af44fd182d2e0a781ff7f352c62ddb46170c0150
JustGimli <aleksnermer@gmail.com>
2022-08-06 20:55:01 +0300

    0.8 beta

--- main_core/static/js/scripts-article.js
+++ main_core/static/js/scripts-article.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    let a = false
+
     $('.a_to_article').mouseenter(function(){
         
         let href = $(this).attr('href')
@@ -8,24 +10,45 @@ $(document).ready(function(){
     })
 
 
-    let a = false
+    
 
     if($('meta').attr('name')=='article'){
         $('.navigator .a-navbar:last-of-type').css({'color': '#1c8784','text-decoration': 'underline'})
     }
 
-    
+    $('.table tr:first-child').click(function(){
 
+        $('.form dl:first-child').animate({
+            left: '25%'
+        })
+
+        $('.form dl:last-child').animate({
+            left: '-150%'
+
+        })
+        
+        
+        
+    })
+    $('.table tr:last-child').click(function(){
+        $('.form dl:first-child').animate({
+            left:'-100%'
+        })
+        $('.form dl:last-child').animate({
+            right:'9%',
+            left:'0%'
+        })
+    })
     $('.add-articles').click(function(){
         if( a === false ){
             if ($(window).width() <='900' ){
-                $('.textarea-1').css('display','none')
+                $('.textarea-1').css({'right':'-150%', 'bottom':'1%', 'text-align':'center'})
                 $('.dl-form-main').css('left','20%')
                 $('.color-42').animate({
                     bottom: '1%'
                 })  
                 $(this).animate({
-                    bottom: '33 %'
+                    bottom: '33%'
                 })
             }else if($(window).width() <= '1400' ){
                 $('.color-42').animate({
@@ -65,7 +88,7 @@ $(document).ready(function(){
             $(this).animate({
                 bottom: '3%'
             })
-            a = true
+
             a = false
         }
         
